30e1866f0f39770cfa0cb184a1b3f2d8f7872d8c
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-01 13:12:20 -0400

    here

---
+++ controllers/thoughtscontroller.js
@@ -0,0 +1,16 @@
+//api/thoughts
+//get to get all thoughts
+//get to get a single thought by _id
+//post to crete a new thought (don't forget to push thoughts id to users thoughts array field)
+
+//json
+//thougthText : ""
+//username :""
+//userId:""
+
+//put to update a thought by id 
+//delete to remove a thought by its id
+
+// api/thoughts/:thoughtId/reactions
+//post to create reaction stored in single thoughts reactions array field
+//delete to pull and remove a reaction by reactionId value
