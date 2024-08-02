184b15346a8944a7d08366d72711c9cb76f74274
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-01 21:42:37 -0400

    finished user controllers

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,3 +1,75 @@
+const { ObjectId }= require('mongoose');
+const { User, Thought } = require('../models')
+
+module.exports ={
+    async getUsers(req,res) {
+        try{
+            const users = await User.find();
+            res.json(users)
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
+    },
+    async getSingleUser(req,res) {
+        try{
+            const user = await User.findOne({_id: req.params.userId})
+            .select('-__v');
+
+            if(!user) {
+                return res.status(404).json({message: "no such user found!"})
+            }
+            res.json(user)
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async createUser(req,res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateUser(req,res) {
+        try{
+            const user = await User.findOneAndUpdate(
+                {_id:req.params.userId},
+                {$set: req.body},
+                {runValidators: true, new: true}
+            );
+            if(!user) {
+                res.status(404).json({message:"No such user exists"})
+            }
+            res.json(user)
+        } catch(err) {
+            res.status(500).json(err);
+        }
+
+    },
+    async deleteUser(req,res) {
+        try {
+            const user = await User.findOneAndRemove({_id: req.params.userId});
+
+            if(!user){
+                return res.status(404).json({message:"No such user exists"})
+            }
+
+            res.json({message:'User successfully deleted'})
+        } catch(err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    // async newFriend(req,res){
+    //     try{
+
+    //     }
+    // }
+}
 // api/users
 //  get all
 //  get user by single _id + friend and thought data
