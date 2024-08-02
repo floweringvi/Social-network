c9d951becd0297aeee19847bd4489bcde3e89885
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-01 21:11:38 -0400

    reaction Schema

--- models/reaction.js
+++ models/reaction.js
@@ -1,3 +1,30 @@
+const {Schema, Types} = require('mongoose');
+reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody :{
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String, 
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    }, 
+    {
+        toJSON:{
+            getters: true,
+        }
+    }
+)
 //reaction
 //just schema
 
