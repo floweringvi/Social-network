791111af9e7aa3adfab36714023bdef378a7c3a2
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-01 22:43:52 -0400

    finished all the thought routes

--- controllers/thoughtscontroller.js
+++ controllers/thoughtscontroller.js
@@ -1,3 +1,107 @@
+const { ObjectId }= require('mongoose');
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts)
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async getSingleThought(req,res) {
+        try{
+            const thought = await Thought.findOne({_id:req.params.thoughtId})
+            .select('-__v');
+
+            if(!thought){
+                return res.status(404).json({message:"No thoughts here!"})
+            }
+            
+            res.json(thought)
+        } catch(err){
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async createThought(req,res) {
+        try{
+            const thought = await Thought.create(req.body);
+
+            const user = await User.findOneAndUpdate(
+                {_id:req.params.userId},
+                {$addToSet: {thoughts: req.body} },
+                {runValidators: true, new: true}
+            )
+            res.json({thought, user})
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateThought(req,res){
+        try{
+            const thought = await Thought.findOneAndUpdate(
+                {_id:req.params.thoughtId},
+                {$set: req.body},
+                {runValidators: true, new: true}
+            );
+            if(!thought){
+               return res.status(404).json({message: "No thoughts here!"})
+            }
+            res.json(thought)
+        } catch(err){
+            res.status(500).json(err);
+        }
+    },
+    async deleteThought (req,res) {
+        try{
+            const thought = await Thought.findOneAndRemove({_id:req.params.thoughtId});
+            
+            if(!thought){
+                return res.status(404).json({message:"No thoughts here!"})
+            }
+            res.json({message:'Thought sucessfully deleted'})
+        } catch(err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }, 
+    async addReaction(req,res){
+        try{
+            const thought = await Thought.findOneAndUpdate(
+                {_id:req.params.id},
+                {$addToSet: {reactions: req.body}},
+                {runValidators:true, new: true},
+            )
+            if(!thought){
+                res.status(404).json({message:"No thoughts here!"})
+            }
+            res.json(thought)
+        } catch(err){
+            console.log(err);
+            res.status(500).json(err)
+        }
+    },
+    async deleteReaction(req,res){
+        try{
+            const thought = await Thought.findOneAndUpdate(
+                {_id:req.params.thoughtId},
+                {$pull: {reaction: {reactionId:req.params.reactionId}}},
+                {runValidators:true, new:true},
+            );
+
+            if (!thought){
+                return res.status(404).json({message:"No thought here!"})
+            }
+            res.json(thought)
+        } catch(err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+}
 //api/thoughts
 //get to get all thoughts
 //get to get a single thought by _id
--- routes/api/thoughtsroutes.js
+++ routes/api/thoughtsroutes.js
@@ -1,3 +1,26 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction, 
+    deleteReaction
+
+} = require('../../controllers/thoughtscontroller.js')
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/reactionId').delete(deleteReaction)
+
 //api/thoughts
 //get to get all thoughts
 //get to get a single thought by _id
