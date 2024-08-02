f65058351467f04d47979694c4d3c57d5c3e07a4
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-01 20:58:59 -0400

    user model

--- models/user.js
+++ models/user.js
@@ -2,27 +2,53 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        username:
+        username: {
+            type: String,
+            required:true,
+        },
         //string
         //unique
         //required
         //trimmed
+        email:{
+            type: String,
+            required:true
+        },
+        thoughts : [
+            {
+                type: Schema.Types.ObjectId,
+                ref:'thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref:'user'
+            }
+        ]
     },
     {
+        toJSON: {
+            virtuals:true,
+        },
         //email
         //string
         //required
         //unique
         //must match a valid email address, mongoose matching validation
-    },
-    {
+    }
+    
         //thoughts
         //array of _id value referencing the 'thought' model
 
-    },
-    {
+    
+    
         //friends 
         //array of _id values referencing the user mdoel (self-reference)
-    }
+    
     //virtual that's called friendCount that retrieves the length of the users'freinds array field of query
-)
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
